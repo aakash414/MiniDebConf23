0ec6c21199ec36b417472f88d0a7c15219799558
AB7zz <abhinavcv007@gmail.com>
2023-10-12 15:03:36 +0530

    button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,11 +19,16 @@ const Hero = () => {
             <img src={logo2} className='m-auto w-[600px]' alt='logo2' />
           </motion.div>
           <div className='md:mt-40 mt-20'>
-            <h1 className='text-[#a80030] text-center m-auto text-5xl md:text-8xl font-[900] ubuntu'>MiniDebConf</h1>
-            <div className='flex justify-center'>
-              <h1 className='text-[#a80030] text-center text-3xl md:text-6xl mr-10 md:mr-40 font-[900] ubuntu'>Kochi'23</h1>
-              <h1 className='text-[#a80030] text-center text-3xl md:text-6xl ubuntu'>CUSAT</h1>
+            <h1 className='text-[#a80030] md:text-left text-center text-5xl md:text-8xl font-[900] ubuntu'>MiniDebConf</h1>
+            <div className='flex justify-center md:justify-start'>
+              <h1 className='text-[#a80030] text-3xl md:text-6xl mr-10 md:mr-40 font-[900] ubuntu'>Kochi'23</h1>
+              <h1 className='text-[#a80030] text-3xl md:text-6xl ubuntu'>CUSAT</h1>
             </div>
+            <motion.div 
+            whileHover={{ scale: 1.05 }}
+            className='my-10 flex md:justify-start justify-center'> 
+              <button className='bg-[#a80030] px-10 py-3 text-white ubuntu font-[800]'>Register</button>
+            </motion.div>
           </div>
         </div>
         <a
