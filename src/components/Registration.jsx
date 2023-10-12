cc4cab35b463ef1c4311dd34cef86ded9389cb0b
AB7zz <abhinavcv007@gmail.com>
2023-10-12 14:56:42 +0530

    luma registration

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Registration = () => {
   return (
-    <div>Registration</div>
+    <div className='my-20 h-[100%] py-10 bg-white'>
+      <h1 className='text-black text-3xl text-center md:text-5xl ubtunu font-[700] mb-10 '>Register</h1>
+      <iframe
+        src="https://lu.ma/embed-checkout/evt-KJFWP5kLbK1uCG5"
+        width="600"
+        height="450"
+        frameborder="0"
+        className="luma w-screen"
+        allowfullscreen=""
+        aria-hidden="false"
+        tabindex="0"
+      ></iframe>
+    </div>
   )
 }
 
