4f68604d6f19e078869278329c12e6bb82be25ea
AB7zz <abhinavcv007@gmail.com>
2023-10-12 13:25:08 +0530

    home setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import About from '../components/About'
+import Timeline from '../components/Timeline'
+import Registration from '../components/Registration'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Navbar />
+      <Hero />
+      <About />
+      <Timeline />
+      <Registration />
+      <Footer />
+    </div>
   )
 }
 
